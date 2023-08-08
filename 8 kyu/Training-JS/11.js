9a2400e65fb972be834f963e1c254c7523319ce7
Ivan Tabola <j.tabola2016@yandex.by>
2023-08-08 21:33:54 +0300

    11.js

---
+++ 8 kyu/Training-JS/11.js
@@ -0,0 +1,19 @@
+function whatNumberIsIt(n){
+    //coding here
+   
+    if (n == Number.MAX_VALUE) {
+      return "Input number is Number.MAX_VALUE";
+    } else if (n == Number.MIN_VALUE) {
+      return "Input number is Number.MIN_VALUE";
+    } else if (n == Number.NaN) {
+      return "Input number is Number.NaN";
+    } else if (n == Number.NEGATIVE_INFINITY) {
+      return "Input number is Number.NEGATIVE_INFINITY";
+    } else if (n == Number.POSITIVE_INFINITY) {
+      return "Input number is Number.POSITIVE_INFINITY";
+    } else if (isNaN(n)) {
+      return "Input number is Number.NaN";
+    } else {
+      return "Input number is " + n;
+    }
+  }
