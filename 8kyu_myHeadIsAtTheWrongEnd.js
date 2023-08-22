c27d0e8b9d79cbe755dbdb48722cb5d4195e4238
IvanTabola <j.tabola2016@yandex.by>
2023-08-22 16:46:03 +0300

    8kyu_myHeadIsAtTheWrongEnd.js

---
+++ 8kyu_myHeadIsAtTheWrongEnd.js
@@ -0,0 +1,4 @@
+function fixTheMeerkat(arr) {
+    return arr.reverse();/*The reverse() method reverses the order of the array elements in place. 
+    The first element of the array becomes the last, and the last one becomes the first.*/
+   }
