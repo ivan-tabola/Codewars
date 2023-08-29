e5496500a1cd3e539f3412dc99883a0f64ebf088
IvanTabola <j.tabola2016@yandex.by>
2023-08-29 19:27:42 +0300

    A bugs trilogy: Episode 1

---
+++ 39_practice.js
@@ -0,0 +1,10 @@
+function yourFutureCareer(career){
+    var career = Math.random()
+       if (career <= 0.32) {
+           return 'FrontEnd Developer';
+   } else if (career <= 0.65) {
+           return 'BackEnd Developer';
+       } else {
+           return 'Full-Stack Developer';
+       }
+ }
