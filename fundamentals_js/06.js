64c18df55d5f62658ebfb5b6e8d4327d9b767053
IvanTabola <j.tabola2016@yandex.by>
2023-08-31 12:47:08 +0300

    static methodName() { ... }

---
+++ fundamentals_js/06.js
@@ -0,0 +1,6 @@
+//Для того, чтобы объявить статический метод класса, необходимо использовать ключевое слово static. Синтаксис static methodName() { ... }
+class Solution {
+    static main() {
+      console.log("Hello World!")
+    }
+  }
