dd4b4f264ac9e3b4f0ae79d4ba2130b530cdfd3d
IvanTabola <j.tabola2016@yandex.by>
2023-09-07 23:52:12 +0300

    Enumerable Magic - Does My List Include This?

---
+++ fundamentals_js/75.js
@@ -0,0 +1,3 @@
+function include(arr, item){
+    return arr.includes(item); //Метод includes() определяет, содержит ли массив определённый элемент, возвращая в зависимости от этого true или false.
+  }
